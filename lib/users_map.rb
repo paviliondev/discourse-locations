2f35562908414c5097c40c86b37f2d0bf0113001
Angus McLeod <angus@mcleod.org.au>
2018-08-10 09:39:47 +1000

    Add site setting for users map limit

--- lib/users_map.rb
+++ lib/users_map.rb
@@ -1,16 +1,16 @@
-MAP_LIMIT = 50
-
 module DirectoryItemsControllerExtension
   def index
     if params[:period] === 'location'
       raise Discourse::InvalidAccess.new(:enable_user_directory) unless SiteSetting.enable_user_directory?
+
+      limit = SiteSetting.location_users_map_limit.to_i
 
       result = DirectoryItem.where("
         user_id in (
           SELECT user_id FROM user_custom_fields WHERE name = 'geo_location'
         )
         AND period_type = 5
-      ").includes(:user).limit(MAP_LIMIT)
+      ").includes(:user).limit(limit)
 
       render_json_dump(directory_items: serialize_data(result, DirectoryItemSerializer))
     else
