027f4c9dfbd44923dd534665d33345d175bc981f
tornike zedginidze <tokozedg@gmail.com>
2019-10-08 16:10:17 +0200

    Add default empty option for country codes array

--- lib/country.rb
+++ lib/country.rb
@@ -2,7 +2,7 @@ class Locations::Country
   def self.codes
     raw_codes = YAML.safe_load(File.read(File.join(Rails.root, 'plugins', 'discourse-locations', 'config', 'country_codes.yml')))
     formatted_codes = []
-
+    formatted_codes.push(code: "", name: "Select Country")
     raw_codes.map do |code, name|
       formatted_codes.push(code: code, name: name)
     end
