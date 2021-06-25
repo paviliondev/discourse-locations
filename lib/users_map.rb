8a16b16298c7d371db6c3dade0722edb38c8179d
Robert Barrow <merefield@gmail.com>
2021-06-25 13:04:10 +0100

    COMPATIBILITY: fix for recent major changes to user directory code

--- lib/users_map.rb
+++ lib/users_map.rb
@@ -12,7 +12,14 @@ module DirectoryItemsControllerExtension
         AND period_type = 5
       ").includes(:user).limit(limit)
 
-      render_json_dump(directory_items: serialize_data(result, DirectoryItemSerializer))
+      serializer_opts = {}
+      serializer_opts[:attributes] = [:likes_received, :likes_given, :topic_count, :post_count, :topics_entered, :posts_read, :days_visited]
+
+
+      serialized = serialize_data(result, DirectoryItemSerializer, serializer_opts)
+      render_json_dump(directory_items: serialized,
+                       meta: {}
+                      )
     else
       super
     end
