c2a031f9f27aa4ed2a35174e83efbcfba413e9cf
merefield <merefield@gmail.com>
2022-12-14 21:38:26 +0000

    evolving tests

--- test/javascripts/acceptance/user-card-test.js
+++ test/javascripts/acceptance/user-card-test.js
@@ -4,9 +4,10 @@ import {
   query,
 } from "discourse/tests/helpers/qunit-helpers";
 import { click, visit } from "@ember/test-helpers";
-import User from "discourse/models/user";
+// import User from "discourse/models/user";
 import { test } from "qunit";
 import userFixtures from "../fixtures/user-fixtures";
+import topicFixtures from "../fixtures/topic-fixtures";
 import { cloneJSON } from "discourse-common/lib/object";
 
 acceptance("User Card - Show Correct User Location Format", function (needs) {
@@ -15,19 +16,28 @@ acceptance("User Card - Show Correct User Location Format", function (needs) {
     location_enabled: true,
     location_user_profile_format: "city|countrycode",
     location_users_map: true,
+    hide_user_profiles_from_public: false
   });
   needs.pretender((server, helper) => {
     const cardResponse = cloneJSON(userFixtures["/u/merefield/card.json"]);
     server.get("/u/merefield/card.json", () => helper.response(cardResponse));
+    const topicResponse = cloneJSON(topicFixtures["/t/51/1.json"]);
+    server.get("/t/51/1.json", () => helper.response(topicResponse));
   });
 
   test("user card location - shows correct format", async function (assert) {
-    console.log('hello this far!');
-    await visit("/t/death-by-poisonous-and-electric-watermelon/41");
-    await click('a[data-user-card="merefield"]');
-
+    await visit("/t/online-learning/51/1");
     assert.equal(
-      query(".user-card .location-label").innerText,
+      query(".small-action-desc.timegap").innerText,
+      "2 years later"
+    );
+    assert.equal(
+      query('a[data-user-card="merefield"]').innerText,
+      ""
+    );
+    await click('a[data-user-card="merefield"]');
+    assert.equal(
+      query(".user-card.location-label").innerText,
       "London, United Kingdom"
     );
   });
