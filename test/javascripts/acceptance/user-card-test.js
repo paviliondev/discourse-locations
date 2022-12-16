9458636beb349126342cf0666005c6f31d60abac
merefield <merefield@gmail.com>
2022-12-16 18:23:03 +0000

    tests: evolved in attempt to add site fixture

--- test/javascripts/acceptance/user-card-test.js
+++ test/javascripts/acceptance/user-card-test.js
@@ -8,7 +8,10 @@ import { click, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import userFixtures from "../fixtures/user-fixtures";
 import topicFixtures from "../fixtures/topic-fixtures";
+import siteFixtures from "../fixtures/site-fixtures";
 import { cloneJSON } from "discourse-common/lib/object";
+import Site from "discourse/models/site";
+import PreloadStore from "discourse/lib/preload-store";
 
 acceptance("User Card - Show Correct User Location Format", function (needs) {
   needs.user();
@@ -19,13 +22,28 @@ acceptance("User Card - Show Correct User Location Format", function (needs) {
     hide_user_profiles_from_public: false
   });
   needs.pretender((server, helper) => {
+    //const siteResponse = cloneJSON(siteFixtures["/site.json"]);
+    // server.get("/site.json", () => {
+    //   debugger;
+    //   helper.response(siteResponse)
+    // });
     const cardResponse = cloneJSON(userFixtures["/u/merefield/card.json"]);
     server.get("/u/merefield/card.json", () => helper.response(cardResponse));
     const topicResponse = cloneJSON(topicFixtures["/t/51/1.json"]);
     server.get("/t/51/1.json", () => helper.response(topicResponse));
   });
 
+
   test("user card location - shows correct format", async function (assert) {
+    const siteAttrs = {
+      ...siteFixtures["site.json"].site,
+    };
+    PreloadStore.store("site", cloneJSON(siteAttrs));
+    
+    
+    Site.resetCurrent();
+    debugger;
+    PreloadStore.get("site")
     await visit("/t/online-learning/51/1");
     assert.equal(
       query(".small-action-desc.timegap").innerText,
