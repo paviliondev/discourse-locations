a5d99b1778bbb1a7bb70f0154b8c162d6bfa3107
merefield <merefield@gmail.com>
2022-12-28 21:49:39 +0000

    clean up front tests

--- test/javascripts/acceptance/user-card-test.js
+++ test/javascripts/acceptance/user-card-test.js
@@ -4,14 +4,11 @@ import {
   query,
 } from "discourse/tests/helpers/qunit-helpers";
 import { click, visit } from "@ember/test-helpers";
-// import User from "discourse/models/user";
 import { test } from "qunit";
 import userFixtures from "../fixtures/user-fixtures";
 import topicFixtures from "../fixtures/topic-fixtures";
 import siteFixtures from "../fixtures/site-fixtures";
 import { cloneJSON } from "discourse-common/lib/object";
-import Site from "discourse/models/site";
-import PreloadStore from "discourse/lib/preload-store";
 
 acceptance("User Card - Show Correct User Location Format", function (needs) {
   needs.user();
@@ -21,27 +18,15 @@ acceptance("User Card - Show Correct User Location Format", function (needs) {
     location_users_map: true,
     hide_user_profiles_from_public: false
   });
+  needs.site(cloneJSON(siteFixtures["country_codes.json"]));
   needs.pretender((server, helper) => {
-    //const siteResponse = cloneJSON(siteFixtures["/site.json"]);
-    // server.get("/site.json", () => {
-    //   debugger;
-    //   helper.response(siteResponse)
-    // });
     const cardResponse = cloneJSON(userFixtures["/u/merefield/card.json"]);
     server.get("/u/merefield/card.json", () => helper.response(cardResponse));
     const topicResponse = cloneJSON(topicFixtures["/t/51/1.json"]);
     server.get("/t/51/1.json", () => helper.response(topicResponse));
   });
 
-
   test("user card location - shows correct format", async function (assert) {
-    const siteAttrs = {
-      ...siteFixtures["site.json"].site,
-    };
-    PreloadStore.store("site", cloneJSON(siteAttrs));
-    Site.resetCurrent();
-    debugger;
-    PreloadStore.get("site")
     await visit("/t/online-learning/51/1");
     assert.equal(
       query(".small-action-desc.timegap").innerText,
@@ -53,7 +38,7 @@ acceptance("User Card - Show Correct User Location Format", function (needs) {
     );
     await click('a[data-user-card="merefield"]');
     assert.equal(
-      query(".user-card.location-label").innerText,
+      query(".user-card .location-label").innerText,
       "London, United Kingdom"
     );
   });
