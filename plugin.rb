7dd66a50b8b35128df17037451a6e6148d5da099
Angus McLeod <angus@mcleod.org.au>
2023-05-09 11:59:44 +0200

    Bump version

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-locations
 # about: Tools for handling locations in Discourse
-# version: 6.2.11
+# version: 6.3.0
 # authors: Angus McLeod, Robert Barrow
 # contact_emails: development@pavilion.tech
 # url: https://github.com/angusmcleod/discourse-locations
