2ebb9328efedc0783e377a08ca2725b7fb95c1d6
merefield <merefield@gmail.com>
2023-10-14 16:37:26 +0100

    wip

--- assets/javascripts/discourse/components/location-form.js
+++ assets/javascripts/discourse/components/location-form.js
@@ -220,6 +220,7 @@ export default class LocationForm extends Component {
         this.loadingLocations = false;
       })
       .catch((error) => {
+        console.log(error)
         this.args.searchError(error);
       });
   }
