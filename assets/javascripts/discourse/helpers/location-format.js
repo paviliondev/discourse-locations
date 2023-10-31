cb3bde10360d2ecef96542bc57f08f106e1dc6b5
Arkshine <github@arkshine.me>
2023-10-31 03:56:47 +0100

    DEV: Register location-format for use with hbr template

--- assets/javascripts/discourse/helpers/location-format.js
+++ assets/javascripts/discourse/helpers/location-format.js
@@ -1,8 +1,9 @@
 import { htmlSafe } from "@ember/template";
-import { helperContext } from "discourse-common/lib/helpers";
+import { helperContext, registerRawHelper } from "discourse-common/lib/helpers";
 import { locationFormat } from "../lib/location-utilities";
 import Site from "discourse/models/site";
 
+registerRawHelper("location-format", _locationFormat);
 export default function _locationFormat(location, opts) {
   let siteSettings = helperContext().siteSettings;
   return htmlSafe(
