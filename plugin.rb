5d0c0169835029d5049c985e94ff5588171db74c
Arkshine <github@arkshine.me>
2023-10-31 01:51:54 +0100

    DEV: Bump version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-locations
 # about: Tools for handling locations in Discourse
-# version: 6.3.13
+# version: 6.3.14
 # authors: Angus McLeod, Robert Barrow
 # contact_emails: development@pavilion.tech
 # url: https://github.com/angusmcleod/discourse-locations
