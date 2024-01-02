30bcd23c150d30d325756e75e6a1cc7b958efbf7
merefield <merefield@gmail.com>
2024-01-02 14:21:03 +0000

    FIX: direct Topic map route

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Locations::Engine.routes.draw do
   get 'validate' => 'geocode#validate'
   get 'countries' => 'geocode#countries'
   get "users_map" => "users_map#index"
-  get 'map_feed' => 'list#map_feed'
 end
 
 Discourse::Application.routes.draw do
+  get 'map_feed' => 'list#map_feed'
   mount ::Locations::Engine, at: 'locations'
 end
