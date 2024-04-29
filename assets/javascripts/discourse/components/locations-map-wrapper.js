8bb446bd7328053373c1d320313c2e53e041a246
merefield <merefield@gmail.com>
2024-04-29 16:14:28 +0100

    FEATURE: add map wrapper to support use in theme components

---
+++ assets/javascripts/discourse/components/locations-map-wrapper.js
@@ -0,0 +1,12 @@
+
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+
+export default class LocationMapWrapperComponent extends Component {
+  @tracked mapType = "";
+
+  constructor() {
+    super(...arguments);
+    this.mapType = this.args.params.mapType;
+  }
+}
