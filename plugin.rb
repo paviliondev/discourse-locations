3c01ccdb7dc2e7172554761508d82bca19100e10
Robert <35533304+merefield@users.noreply.github.com>
2024-11-10 14:46:26 +0000

    Bump patch

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-locations
 # about: Tools for handling locations in Discourse
-# version: 6.6.14
+# version: 6.6.15
 # authors: Angus McLeod, Robert Barrow
 # contact_emails: development@pavilion.tech
 # url: https://github.com/angusmcleod/discourse-locations
