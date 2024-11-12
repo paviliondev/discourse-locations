4f747c0b3fa9a45d23f1c40a105ab13479ad1680
merefield <merefield@gmail.com>
2024-11-12 15:52:55 +0000

    bump patch

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-locations
 # about: Tools for handling locations in Discourse
-# version: 6.6.17
+# version: 6.7.0
 # authors: Angus McLeod, Robert Barrow
 # contact_emails: development@pavilion.tech
 # url: https://github.com/angusmcleod/discourse-locations
